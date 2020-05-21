4732720a009b3e9d5ea05d5a530b7479fef07bc0
Justyna Nowakowska <nowakowska.just@gmail.com>
2020-05-21 20:57:16 +0200

    Add main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+numbers=range(1,100)
+podzielne=[]
+potega=[]
+
+for number in numbers:
+    if (number % 5 ==0):
+        potęga=number**3
+        podzielne.append(number)
+        potega.append(number**3)
+
+print(podzielne,"\n",potega)
+
